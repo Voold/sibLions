e7acd8560a5c20890569286f5cd9ac70522f6091
Voold <barsik.koteevich@mail.ru>
2026-03-04 09:14:06 +0700

    oauth2.0 | last redirect url update

--- src/config/oauth.ts
+++ src/config/oauth.ts
@@ -7,7 +7,7 @@ dotenv.config({ path: path.resolve(process.cwd(), 'src', 'config', '.env') });
 export const oauthConfig = {
   clientId: process.env.OAUTH_CLIENT_ID,
   clientSecret: process.env.OAUTH_CLIENT_SECRET,
-  redirectUri: 'https://songeng.voold.online/auth/callback',
+  redirectUri: 'https://songeng.voold.online/api/auth/callback',
   authEndpoint: 'https://oauth.tpu.ru/authorize',
   tokenEndpoint: 'https://oauth.tpu.ru/access_token',
   getUserInfoEndpoint: 'https://oauth.tpu.ru/user',
