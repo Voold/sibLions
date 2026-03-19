9b5abf23d42c6b95eeb69055441a019920578e29
Voold <barsik.koteevich@mail.ru>
2026-03-19 09:17:44 +0700

    Corses for debug

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,10 @@ const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
 // Мидлвары
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(requestLogger);
 
