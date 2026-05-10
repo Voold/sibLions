c999c52978706dcd3b694552b2616198d5c97b68
Voold <barsik.koteevich@mail.ru>
2026-05-10 16:47:58 +0700

    CORS updated

--- src/server.ts
+++ src/server.ts
@@ -20,10 +20,16 @@ const PORT = process.env.PORT || 3000;
 // Мидлвары
 app.use(
   cors({
-    origin: "https://localhost:5173",
+    origin: ["http://localhost:5173", "https://localhost:5173"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "apiKey"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "apiKey",
+      "Accept",
+      "X-Requested-With",
+    ],
   }),
 );
 app.use(express.json());
